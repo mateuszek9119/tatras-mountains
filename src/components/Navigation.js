a088f7cf9d8a6022513b08b4d9b5f892718b4d9d
mateusz <mateusz@gmail.com>
2024-02-11 19:07:30 +0100

    response nav change

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,32 +1,32 @@
 import { NavLink } from "react-router-dom"
 import '../styles/navigation.css'
 
-const Navigation = (props)=>{
+const Navigation = (props) => {
 
-    const hideNavigation = ()=>{ 
-        
-        if(window.innerWidth < 900){
+    const hideNavigation = () => {
+
+        if (window.innerWidth < 900) {
             props.click()
-            window.scroll(0,0)
-        } 
-    }       
+            window.scroll(0, 0)
+        }
+    }
 
     const li = [
-        {adres: "/" , name: "Strona Główna" },
-        {adres: "/products" , name: "Szczyty i szlaki" },
-        {adres: "/weather" , name: "Pogoda" },
-        {adres: "/contact" , name: "Kontakt" },
-        {adres: "/admin" , name: "Panel Admina" }
+        { adres: "/", name: "Strona Główna" },
+        { adres: "/products", name: "Szczyty i szlaki" },
+        { adres: "/weather", name: "Pogoda" },
+        { adres: "/contact", name: "Kontakt" },
+        { adres: "/admin", name: "Panel Admina" }
     ]
 
-    const nav = li.map( item => <li key={item.name} onClick={hideNavigation }>  <NavLink to={item.adres} > {item.name} </NavLink> </li>)
+    const nav = li.map(item => <li key={item.name} onClick={hideNavigation}>  <NavLink to={item.adres} > {item.name} </NavLink> </li>)
 
-    return(
-            <nav className="main">
-                <ul>
-                    { nav }
-                </ul>
-            </nav>
+    return (
+        <nav className="main">
+            <ul>
+                {nav}
+            </ul>
+        </nav>
     )
 }
 
